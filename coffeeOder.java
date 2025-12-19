d5735864d81d5b78ef189c09886370dacfda06e0
munyirichrispine <munyirichrispine@gmail.com>
2025-12-19 13:45:04 +0300

    Create coffeeOder.java

---
+++ coffeeOder.java
@@ -0,0 +1,29 @@
+//chrispine munyiri
+//J77-10543-2024
+class CoffeeOrder {
+    String size;
+    double price;
+
+    // Constructor
+    public CoffeeOrder(String size, double price) {
+        this.size = size;
+        this.price = price;
+    }
+
+    // Method to display order details
+    public void displayOrder() {
+        System.out.println("Coffee Size: " + size + ", Price: " + price);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        // Creating objects using constructor
+        CoffeeOrder order1 = new CoffeeOrder("Large", 250.0);
+        CoffeeOrder order2 = new CoffeeOrder("Medium", 180.0);
+
+        // Displaying details
+        order1.displayOrder();
+        order2.displayOrder();
+    }
+}
